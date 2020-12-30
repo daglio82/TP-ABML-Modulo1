fe635077f85fb425b57dc2112a3466f8d38eeb0c
daglio82 <53617990+daglio82@users.noreply.github.com>
2020-12-30 10:10:24 -0300

    Add files via upload

--- guardar.php
+++ guardar.php
@@ -5,7 +5,10 @@ include('db.php');
 if (isset($_POST['guardar'])) {
   $nombre = $_POST['nombre'];
   $apellido = $_POST['apellido'];
-  $query = "INSERT INTO socio(nombre, apellido) VALUES ('$nombre', '$apellido')";
+  $direccion = $_POST['direccion'];
+  $telefono = $_POST['telefono'];
+  $grupo_familair = $_POST['grupo_familair'];
+  $query = "INSERT INTO socio(nombre, apellido, direccion, telefono, grupo_familair) VALUES ('$nombre', '$apellido', '$direccion', '$telefono', '$grupo_familair')";
   $result = mysqli_query($mysqli, $query);
   if(!$result) {
     die("Query Failed.");
--- header.php
+++ header.php
@@ -10,9 +10,11 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   </head>
   <body>
-
+    
     <nav class="navbar navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="index.php">Club Social y Deportivo CRUD</a>
       </div>
     </nav>
+    
+  <body>
